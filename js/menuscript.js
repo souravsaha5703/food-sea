2acf3b09e42941f4362ab02a186446b918c59a6d
Sourav Saha <saha83787@gmail.com>
2023-11-16 21:36:05 +0530

    food menu design completed

---
+++ js/menuscript.js
@@ -0,0 +1,174 @@
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+
+var cursor=document.getElementById("cursor");
+var homeBtn=document.getElementById("home");
+var foodMenuBtn=document.getElementById("menu");
+var bookTableBtn=document.getElementById("booktable");
+var onlineBookBtn=document.getElementById("onlineorder");
+var aboutBtn=document.getElementById("about");
+var contactBtn=document.getElementById("contact");
+var menuBtn=document.getElementById("menubar");
+var menuSection=document.getElementsByClassName("menu-section");
+var closeMenuBtn=document.getElementById("close-menu");
+
+document.addEventListener("mousemove",(dets)=>{
+    cursor.style.left=dets.x+"px";
+    cursor.style.top=dets.y+"px";
+    cursor.style.transition="all 0.3 ease-in";
+});
+
+homeBtn.addEventListener("mouseenter",()=>{
+    gsap.to(cursor,{
+        width:30,
+        height:30
+    });
+    gsap.to(homeBtn,{
+        fontSize:50
+    });
+});
+homeBtn.addEventListener("mouseleave",()=>{
+    homeBtn.style.fontSize="2rem";
+    gsap.to(cursor,{
+        width:10,
+        height:10
+    });
+    gsap.to(homeBtn,{
+        fontSize:32
+    });
+});
+
+foodMenuBtn.addEventListener("mouseenter",()=>{
+    gsap.to(cursor,{
+        width:30,
+        height:30
+    });
+    gsap.to(foodMenuBtn,{
+        fontSize:50
+    });
+});
+foodMenuBtn.addEventListener("mouseleave",()=>{
+    gsap.to(cursor,{
+        width:10,
+        height:10
+    });
+    gsap.to(foodMenuBtn,{
+        fontSize:32
+    });
+});
+
+bookTableBtn.addEventListener("mouseenter",()=>{
+    gsap.to(cursor,{
+        width:30,
+        height:30
+    });
+    gsap.to(bookTableBtn,{
+        fontSize:50
+    });
+});
+bookTableBtn.addEventListener("mouseleave",()=>{
+    gsap.to(cursor,{
+        width:10,
+        height:10
+    });
+    gsap.to(bookTableBtn,{
+        fontSize:32
+    });
+});
+
+onlineBookBtn.addEventListener("mouseenter",()=>{
+    gsap.to(cursor,{
+        width:30,
+        height:30
+    });
+    gsap.to(onlineBookBtn,{
+        fontSize:50
+    });
+});
+onlineBookBtn.addEventListener("mouseleave",()=>{
+    gsap.to(cursor,{
+        width:10,
+        height:10
+    });
+    gsap.to(onlineBookBtn,{
+        fontSize:32
+    });
+});
+
+aboutBtn.addEventListener("mouseenter",()=>{
+    gsap.to(cursor,{
+        width:30,
+        height:30
+    });
+    gsap.to(aboutBtn,{
+        fontSize:50
+    });
+});
+aboutBtn.addEventListener("mouseleave",()=>{
+    gsap.to(cursor,{
+        width:10,
+        height:10
+    });
+    gsap.to(aboutBtn,{
+        fontSize:32
+    });
+});
+
+contactBtn.addEventListener("mouseenter",()=>{
+    gsap.to(cursor,{
+        width:30,
+        height:30
+    });
+    gsap.to(contactBtn,{
+        fontSize:50
+    });
+});
+contactBtn.addEventListener("mouseleave",()=>{
+    gsap.to(cursor,{
+        width:10,
+        height:10
+    });
+    gsap.to(contactBtn,{
+        fontSize:32
+    });
+});
+
+menuBtn.addEventListener("mouseenter",()=>{
+    gsap.to(menuBtn,{
+        rotation:90
+    });
+});
+
+menuBtn.addEventListener("mouseleave",()=>{
+    gsap.to(menuBtn,{
+        rotation:180
+    });
+});
+
+menuBtn.addEventListener("click",()=>{
+    gsap.to(menuSection,{
+        right:0,
+        ease:Power3
+    });
+});
+
+closeMenuBtn.addEventListener("mouseenter",()=>{
+    gsap.to(closeMenuBtn,{
+        rotation:90
+    });
+});
+
+closeMenuBtn.addEventListener("mouseleave",()=>{
+    gsap.to(closeMenuBtn,{
+        rotation:-90
+    });
+});
+
+closeMenuBtn.addEventListener("click",()=>{
+    gsap.to(menuSection,{
+        right:"-50%",
+        ease:Power3
+    });
+});
